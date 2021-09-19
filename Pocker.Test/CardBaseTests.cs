9a9fd4b06fdd1ab3b418d4619c41f0686d4840b7
HiveBeats <38073817+HiveBeats@users.noreply.github.com>
2021-09-19 20:38:56 +0700

    unit tests

---
+++ Pocker.Test/CardBaseTests.cs
@@ -0,0 +1,31 @@
+﻿using Pocker.Cards;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace Pocker.Test
+{
+	public class CardBaseTests
+	{
+        [Fact]
+        public void Name_Should_Exist()
+        {
+            var card = new CardBase(Rank.Jack, Suit.Heart);
+            
+            string cardName = card.Name;
+
+            Assert.False(string.IsNullOrWhiteSpace(cardName));
+        }
+
+        [Fact]
+        public void Name_Should_Be_Right()
+        {
+            var card = new CardBase(Rank.Jack, Suit.Heart);
+
+            string cardName = card.Name;
+
+            Assert.Equal("Валет Черви", cardName);
+        }
+    }
+}
